7f2bf53b9866e503c14c526c3e3bf442c61b08c8
Brian Zanti <brianzanti@gmail.com>
2020-09-17 11:08:44 -0600

    completes all challenges

--- apis.rb
+++ apis.rb
@@ -1,8 +1,40 @@
 require 'faraday'
 require 'pry'
 require 'json'
+require './film'
+require './person'
+require './ghibili_service'
 
-response = Faraday.get("https://api.github.com/users/brianzanti")
-json = JSON.parse(response.body, symbolize_names: true)
+class Ghibili
+  def show_all_films
+    json = GhibiliService.new.all_films
 
-binding.pry
+    films = json.map do |film_hash|
+      Film.new(film_hash)
+    end
+
+    films.each do |film|
+      puts film.title
+    end
+  end
+
+  def show_all_people
+    json = GhibiliService.new.all_people
+
+    people = json.map do |people_hash|
+      Person.new(people_hash)
+    end
+
+    people.each do |person|
+      puts person.name
+      puts 'films they appear in:'
+      person.films.each do |film|
+        puts film.title
+      end
+      puts "================="
+    end
+  end
+end
+
+Ghibili.new.show_all_films
+Ghibili.new.show_all_people
